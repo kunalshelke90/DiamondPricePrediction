c2859223d058acda9fdd24a31f98b0d449283001
kunalshelke90 <shelkekunal90@gmail.com>
2024-01-06 18:23:55 +0530

    model_evaulatoin updated

--- README.md
+++ README.md
@@ -24,3 +24,38 @@ git pull
 ```
 pip install -r requirements.txt
 ```
+
+## MLflow
+
+[Documentation](https://mlflow.org/docs/latest/index.html)
+
+
+##### local cmd
+- mlflow ui
+
+### dagshub
+[dagshub](https://dagshub.com/)
+
+MLFLOW_TRACKING_URI=https://dagshub.com/shelkekunal90/fsdsmendtoend.mlflow 
+MLFLOW_TRACKING_USERNAME=shelkekunal90 
+MLFLOW_TRACKING_PASSWORD=654cde79efc37a8d0050cdfa4da919e4664891e4 
+python script.py
+
+Run this to export as env variables:
+
+```bash
+
+export MLFLOW_TRACKING_URI=https://dagshub.com/shelkekunal90/fsdsmendtoend.mlflow
+
+export MLFLOW_TRACKING_USERNAME=shelkekunal90
+
+export MLFLOW_TRACKING_PASSWORD=654cde79efc37a8d0050cdfa4da919e4664891e4
+
+```
+
+
+### DVC cmd
+- dvc init
+- dvc repro
+- dvc dag
+
--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -7,13 +7,7 @@ from src.DimondPricePrediction.components.data_transformation import DataTransfo
 
 from src.DimondPricePrediction.components.model_trainer import ModelTrainer
 
-#from src.DimondPricePrediction.components.model import ModelEvaluation
-
-import os
-
-from src.DimondPricePrediction.logger import logging
-from src.DimondPricePrediction.exception import customexception
-import pandas as pd
+from src.DimondPricePrediction.components.model_evaluation import ModelEvaluation
 
 obj=DataIngestion()
 
@@ -28,5 +22,5 @@ model_trainer_obj=ModelTrainer()
 model_trainer_obj.initate_model_training(train_arr,test_arr)
 
 
-#model_eval_obj = ModelEvaluation()
-#model_eval_obj.initiate_model_evaluation(train_arr,test_arr)
+model_eval_obj = ModelEvaluation()
+model_eval_obj.initiate_model_evaluation(train_arr,test_arr)
