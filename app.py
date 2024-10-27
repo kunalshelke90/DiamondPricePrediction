282f598e6590912dad159cae69d5e4754cc57be8
kunalshelke90 <shelkekunal90@gmail.com>
2024-10-27 17:58:48 +0530

    updated file

--- app.py
+++ app.py
@@ -1,25 +1,18 @@
-
 from src.DiamondPricePrediction.pipelines.prediction_pipeline import CustomData,PredictPipeline
-
-from flask import Flask,request,render_template,jsonify
-
+from flask import Flask,request,render_template
 
 app=Flask(__name__)
-
 
 @app.route('/')
 def home_page():
     return render_template("index.html")
 
-
 @app.route("/predict",methods=["GET","POST"])
 def predict_datapoint():
     if request.method == "GET":
-        return render_template("form.html")
-    
+        return render_template("form.html")    
     else:
         data=CustomData(
-            
             carat=float(request.form.get('carat')),
             depth = float(request.form.get('depth')),
             table = float(request.form.get('table')),
@@ -36,11 +29,9 @@ def predict_datapoint():
         predict_pipeline=PredictPipeline()
         
         pred=predict_pipeline.predict(final_data)
-        
         result=round(pred[0],2)
         
         return render_template("result.html",final_result=result)
 
-#execution begin
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=8080)
+    app.run(host="0.0.0.0",port=8080,debug=True)
