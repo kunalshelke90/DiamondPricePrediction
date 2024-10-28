57dde71059bb150defe6fb81064c61fa2977477a
kunalshelke90 <shelkekunal90@gmail.com>
2024-10-28 14:01:20 +0530

    file updated

--- src/DiamondPricePrediction/components/data_ingestion.py
+++ src/DiamondPricePrediction/components/data_ingestion.py
@@ -1,36 +1,25 @@
 import pandas as pd
-import numpy as np
 from src.DiamondPricePrediction.logger import logging
 from src.DiamondPricePrediction.exception import customexception
 import os
 import sys
 from sklearn.model_selection import train_test_split
-from dataclasses import dataclass
 from pathlib import Path
-
-
-sys.path.append("c:\\DS\\fsdsmendtoend")
-
-
 
 class DataIngestionConfig:
     raw_data_path:str=os.path.join("artifacts","raw.csv")
     train_data_path:str=os.path.join("artifacts","train.csv")
     test_data_path:str=os.path.join("artifacts","test.csv")
 
-
 class DataIngestion:
     def __init__(self):
         self.ingestion_config=DataIngestionConfig()
         
-    
     def initiate_data_ingestion(self):
         logging.info("data ingestion started")
-        
         try:
             data=pd.read_csv(Path(os.path.join("notebooks/data","gemstone.csv")))
             logging.info(" i have read dataset as a df")
-            
             
             os.makedirs(os.path.dirname(os.path.join(self.ingestion_config.raw_data_path)),exist_ok=True)
             data.to_csv(self.ingestion_config.raw_data_path,index=False)
@@ -46,14 +35,9 @@ class DataIngestion:
             
             logging.info("data ingestion part completed")
             
-            return (
-                 
-                
-                self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path
-            )
-            
-            
+            return (self.ingestion_config.train_data_path,
+                    self.ingestion_config.test_data_pat)
+    
         except Exception as e:
            logging.info("exception during occured at data ingestion stage")
            raise customexception(e,sys)
